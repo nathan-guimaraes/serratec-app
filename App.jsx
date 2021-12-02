6e0639a9247c5b5db203cd93580e293a468b0bc6
Yago Azevedo <yago.dev@gmail.com>
2021-12-01 21:45:58 -0300

    aula 3

--- App.jsx
+++ App.jsx
@@ -1,27 +1,55 @@
-import React, { useState } from 'react';
-import { Text, StatusBar, TextInput } from 'react-native';
-import Title from './components/Title';
-import { Container } from './components/Container';
+import "react-native-gesture-handler";
+import { Box, NativeBaseProvider, Input, Button } from "native-base";
+import React, { useState } from "react";
+import { StatusBar } from "react-native";
+import { Container } from "./components/Container";
+import MyTextInput from "./components/MyTextInput";
+import Title from "./components/Title";
 
 export default function App() {
-  const [meuTexto, setMeuTexto] = useState();
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
   return (
-    <Container>
-      <Title>Serratec app</Title>
-      <Text style={{color: 'red'}}>
-        Olá Mundo!
-      </Text>
-      <TextInput
-        onChangeText={setMeuTexto}
-        value={meuTexto}
-        placeholder="Digite qualquer coisa"
-        keyboardType="numeric"
-      />
-      <StatusBar
-        backgroundColor="blue"
-        style="light"
-        barStyle="dark-content"
-      />
-    </Container>
+    <NativeBaseProvider>
+      <Container>
+        <Title>Serratec app</Title>
+        <Input
+          mx="3"
+          placeholder="Seu e-mail"
+          w={{
+            base: "80%",
+            md: "25%",
+          }}
+          style={{marginTop: 20}}
+          onChangeText={setEmail}
+          value={email}
+          keyboardType="default"
+        />
+        <Input
+          mx="3"
+          placeholder="Sua senha"
+          w={{
+            base: "80%",
+            md: "25%",
+          }}
+          style={{margin: 20}}
+          onChangeText={setSenha}
+          value={senha}
+          type="password"
+        />
+        <Button
+          size="lg"
+          variant="outline"
+          onPress={() => console.log("clicou aqui")}
+        >
+          Login
+        </Button>
+        <StatusBar
+          backgroundColor="blue"
+          style="light"
+          barStyle="dark-content"
+        />
+      </Container>
+    </NativeBaseProvider>
   );
 }
